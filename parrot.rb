637c9bad6b723d249231c72db17c43063f4a873c
Patricia C <patricia@patriciaco.com>
2020-01-27 07:12:01 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,27 +1,49 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+#Create method `parrot` that outputs a given phrase and
+#returns the phrase
 
 #def parrot
 #puts "Squawk!"
 #end
-parrot
+#parrot
 
 #def parrot
- #puts "Squawk!"
+#puts "Squawk!"
 #end
 
 #def parrot
-  #puts "Squawk!"
-  #"Squawk!"
+#puts "Squawk!"
+#"Squawk!"
 #end
 #parrot
 
-def parrot
- noise = "animal"
-  return "Squawk!"
-  "animal"
+#def parrot
+#puts "Squawk!"
+#return parrot
+#end
+#parrot
+
+#def parrot
+#puts "Squawk!"
+#end
+
+#def parrot 
+  #phrase = parrot("Squawk!")
+  #puts phrase
+  #return "Squawk!"
+ #end
+ 
+
+
+def parrot(phrase = "Squawk!")
+puts phrase
+return phrase
 end
 parrot
+
+
+
+
+
 
 
 
